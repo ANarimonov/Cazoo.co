54b8e84d9904f6b7dd4ce64528b3c6b5c3fd6320
Abdurahmon Narimonov <abdurrohmannarimonov@gmail.com>
2023-05-08 14:14:00 +0500

    add cors config

--- src/main/java/com/anarimonov/cazoo/CazooApplication.java
+++ src/main/java/com/anarimonov/cazoo/CazooApplication.java
@@ -2,12 +2,21 @@ package com.anarimonov.cazoo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class CazooApplication {
+public class CazooApplication implements WebMvcConfigurer{
 
     public static void main(String[] args) {
         SpringApplication.run(CazooApplication.class, args);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 
 }
