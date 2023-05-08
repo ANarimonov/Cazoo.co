15b1ec6cdafe21eeef641714f5896489752a51e6
Abdurahmon Narimonov <abdurrohmannarimonov@gmail.com>
2023-05-08 14:14:23 +0500

    bug fix

--- src/main/java/com/anarimonov/cazoo/CazooApplication.java
+++ src/main/java/com/anarimonov/cazoo/CazooApplication.java
@@ -2,6 +2,7 @@ package com.anarimonov.cazoo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
