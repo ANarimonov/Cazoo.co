38df0ca4f054c4687f80297d503038282988fcfa
Abdurahmon Narimonov <abdurrohmannarimonov@gmail.com>
2023-05-16 18:14:39 +0500

    -Bug fixes and other minor improvements

--- src/main/java/com/anarimonov/cazoo/repository/MakerRepository.java
+++ src/main/java/com/anarimonov/cazoo/repository/MakerRepository.java
@@ -2,8 +2,11 @@ package com.anarimonov.cazoo.repository;
 
 import com.anarimonov.cazoo.entity.Maker;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 public interface MakerRepository extends JpaRepository<Maker, Long> {
+    @Query(nativeQuery = true, value = "select * from makers join cars c on makers.id = c.maker_id where c.id = :carId")
+    Maker getByCarId(long carId);
 }
--- src/main/java/com/anarimonov/cazoo/repository/ModelRepository.java
+++ src/main/java/com/anarimonov/cazoo/repository/ModelRepository.java
@@ -2,9 +2,13 @@ package com.anarimonov.cazoo.repository;
 
 import com.anarimonov.cazoo.entity.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ModelRepository extends JpaRepository<Model, Long> {
     List<Model> findByMakerId(long maker_id);
+
+    @Query(nativeQuery = true, value = "select * from models join cars c on models.id = c.model_id where c.id = :carId")
+    Model getByCarId(long carId);
 }
