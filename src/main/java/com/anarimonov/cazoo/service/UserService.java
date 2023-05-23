5405311774ba0e39158839fb883715a5f53f4228
Abdurahmon Narimonov <abdurrohmannarimonov@gmail.com>
2023-05-23 21:14:34 +0500

    -Bug fixes and other minor improvements

--- src/main/java/com/anarimonov/cazoo/service/UserService.java
+++ src/main/java/com/anarimonov/cazoo/service/UserService.java
@@ -7,11 +7,13 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
+@Service
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
 
